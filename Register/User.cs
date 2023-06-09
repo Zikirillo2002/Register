06791b42479e30392e6c022098a8c5e121c12966
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2023-06-09 07:54:43 +0500

    Update User.cs

--- Register/User.cs
+++ Register/User.cs
@@ -8,5 +8,10 @@ namespace Register
 {
     internal class User
     {
+        public string Fullname { get; set; }
+        public string Username { get; set; }
+        public string Phonenumber { get; set; }
+        public string Pasword { get; set; }
+        public Dictionary<string, StreamReader> UserList = new Dictionary<string, StreamReader>();
     }
 }
